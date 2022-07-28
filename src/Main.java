39698bcbf72716da26e13e92598bdbe800cd392d
Anatol Pronski <tocker2000by@yahoo.com>
2022-07-28 22:31:45 +0300

    'header-changed2'

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введенное число будет разложено на множители (целые числа)");
+        System.out.println("Введенное число будет разложено на множители");
         int value = new Scanner(System.in).nextInt();
 
 
